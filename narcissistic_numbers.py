266aece07f0d3e91a64f7820a67a79e39a2ca26b
Petya Shopska <145231798+petyaradoslavova@users.noreply.github.com>
2024-01-25 20:33:04 +0200

    Update narcissistic_numbers.py

--- narcissistic_numbers.py
+++ narcissistic_numbers.py
@@ -1,4 +1,5 @@
 def narcissistic(value):
+    """Find if a number is narcissistic number - sum of its digits each raised to the power of the number of digits"""
     sum_of_digits = sum(int(digit) ** len(str(value)) for digit in str(value))
     if sum_of_digits == value:
         return True
